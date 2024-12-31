9c71ab794089afe1dfbb8a019b752c4bdb2b71ae
JH Park <harryest@naver.com>
2024-12-31 22:38:02 +0900

    Feat: Initial Commit

---
+++ src/main/java/com/backend/DuruDuru/DuruDuruApplication.java
@@ -0,0 +1,14 @@
+package com.backend.DuruDuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuruDuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuruDuruApplication.class, args);
+		System.out.println("[Initiate UMC Project DuruDuru]");
+	}
+
+}
---
+++ src/test/java/com/backend/DuruDuru/DuruDuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.DuruDuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuruDuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
