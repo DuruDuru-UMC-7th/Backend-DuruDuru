02dc349de50ccec4ce1067d9f6d02bbca31985a8
JH Park <harryest@naver.com>
2025-01-25 22:20:15 +0900

    Refactor: 대분류, 소분류 카테고리 Enum 추가

---
+++ src/main/java/com/backend/DuruDuru/global/domain/enums/MajorCategory.java
@@ -0,0 +1,17 @@
+package com.backend.DuruDuru.global.domain.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum MajorCategory {
+    과일,
+    육류,
+    수산물,
+    달걀,
+    유제품,
+    채소,
+    버섯,
+    두부,
+    건조식품,
+    기타;
+}
