9f7463787134430101c0ef28769a2a435e6088fc
bingseok <qqq7384@naver.com>
2025-02-11 22:32:01 +0900

    Refactor: 레시피 즐겨찾기 시 한글저장되도록 수정

--- src/main/java/com/backend/DuruDuru/global/domain/entity/MemberRecipe.java
+++ src/main/java/com/backend/DuruDuru/global/domain/entity/MemberRecipe.java
@@ -21,7 +21,7 @@ public class MemberRecipe extends BaseEntity {
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @Column(name = "recipe_name", nullable = false , columnDefinition = "varchar(50)")
+    @Column(name = "recipe_name", nullable = false, length = 255, columnDefinition = "varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
     private String recipeName;
 
     public void setMember(Member member) {
