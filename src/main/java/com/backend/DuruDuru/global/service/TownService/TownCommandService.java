946b59a50246021c66e98a3b0ebe805b77b55508
jkjade <jkjade@naver.com>
2025-02-14 13:21:34 +0900

    Refactor: Trade 관련 API AuthUser 인증으로 수정

--- src/main/java/com/backend/DuruDuru/global/service/TownService/TownCommandService.java
+++ src/main/java/com/backend/DuruDuru/global/service/TownService/TownCommandService.java
@@ -5,10 +5,8 @@ import com.backend.DuruDuru.global.domain.entity.Town;
 import com.backend.DuruDuru.global.web.dto.Town.TownRequestDTO;
 
 public interface TownCommandService {
-
-    // Town 엔티티를 저장하는 메서드
+    // Town 엔티티 저장
     Town createTown(Member member, TownRequestDTO.ToTownRequestDTO request);
-    // Town 정보를 수정하는 메서드
+    // Town 정보 수정
     Town updateTown(Member member, TownRequestDTO.ToTownRequestDTO request);
-
 }
--- src/main/java/com/backend/DuruDuru/global/web/controller/TradeController.java
+++ src/main/java/com/backend/DuruDuru/global/web/controller/TradeController.java
@@ -5,14 +5,17 @@ import com.backend.DuruDuru.global.apiPayload.ApiResponse;
 import com.backend.DuruDuru.global.apiPayload.code.status.SuccessStatus;
 import com.backend.DuruDuru.global.converter.TradeConverter;
 import com.backend.DuruDuru.global.domain.entity.LikeTrade;
+import com.backend.DuruDuru.global.domain.entity.Member;
 import com.backend.DuruDuru.global.domain.entity.Trade;
 import com.backend.DuruDuru.global.domain.enums.Status;
 import com.backend.DuruDuru.global.domain.enums.TradeType;
+import com.backend.DuruDuru.global.security.handler.annotation.AuthUser;
 import com.backend.DuruDuru.global.service.TradeService.TradeCommandService;
 import com.backend.DuruDuru.global.service.TradeService.TradeQueryService;
 import com.backend.DuruDuru.global.web.dto.Trade.TradeRequestDTO;
 import com.backend.DuruDuru.global.web.dto.Trade.TradeResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,11 +41,11 @@ public class TradeController {
     @PostMapping(value = "/", consumes = "multipart/form-data")
     @Operation(summary = "품앗이 게시글 등록 API", description = "품앗이 게시글을 등록하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradeDetailResultDTO> createTrade(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @RequestParam Long ingredientId,
             @ModelAttribute TradeRequestDTO.CreateTradeRequestDTO request
     ){
-        Trade trade = tradeCommandService.createTrade(memberId, ingredientId, request);
+        Trade trade = tradeCommandService.createTrade(member, ingredientId, request);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_CREATE_OK, TradeConverter.toTradeDetailDTO(trade));
     }
 
@@ -50,9 +53,10 @@ public class TradeController {
     @GetMapping("/{trade_id}")
     @Operation(summary = "품앗이 게시글 상세 조회 API", description = "특정 품앗이 게시글을 상세 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradeDetailResultDTO> findTradeById(
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_id") Long tradeId
     ){
-        Trade trade = tradeQueryService.getTrade(tradeId);
+        Trade trade = tradeQueryService.getTrade(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_DETAIL_OK, TradeConverter.toTradeDetailDTO(trade));
     }
 
@@ -60,10 +64,10 @@ public class TradeController {
     @DeleteMapping("/{trade_id}")
     @Operation(summary = "품앗이 게시글 삭제 API", description = "특정 품앗이를 삭제하는 API 입니다.")
     public ApiResponse<?> deleteTrade(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_id") Long tradeId
     ){
-        tradeCommandService.deleteTrade(memberId, tradeId);
+        tradeCommandService.deleteTrade(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_DELETE_OK, null);
     }
 
@@ -72,7 +76,7 @@ public class TradeController {
     @Operation(summary = "품앗이 게시글 수정 API", description = "특정 품앗이를 수정하는 API 입니다. 삭제할 이미지는 해당 이미지의 id값으로 넣어주세요.")
     public ApiResponse<TradeResponseDTO.TradeDetailResultDTO> updateTrade(
             @PathVariable("trade_id") Long tradeId,
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @RequestParam(required = false) List<Long> deleteImgIds,
             @RequestParam(value = "addImgs", required = false) List<MultipartFile> addImgs, // 이미지 리스트 처리
             @RequestParam(required = false) TradeType tradeType,
@@ -81,7 +85,7 @@ public class TradeController {
             @RequestParam(required = false) Long ingredientCount
     ){
         TradeRequestDTO.UpdateTradeRequestDTO request = new TradeRequestDTO.UpdateTradeRequestDTO(ingredientCount, body, tradeType, status, deleteImgIds, addImgs);
-        Trade trade = tradeCommandService.updateTrade(memberId, tradeId, request);
+        Trade trade = tradeCommandService.updateTrade(member, tradeId, request);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_UPDATE_OK, TradeConverter.toTradeDetailDTO(trade));
     }
 
@@ -89,9 +93,9 @@ public class TradeController {
     @GetMapping("/my/active")
     @Operation(summary = "나의 활성화 품앗이 리스트 조회 API", description = "나의 활성화된 품앗이 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findCurrentTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ){
-        List<Trade> tradeList = tradeQueryService.getActiveTradesByMember(memberId);
+        List<Trade> tradeList = tradeQueryService.getActiveTradesByMember(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -99,9 +103,9 @@ public class TradeController {
     @GetMapping("/my/history")
     @Operation(summary = "나의 전체 품앗이 리스트 조회 API", description = "나의 전체 품앗이 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findHistoryTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ){
-        List<Trade> tradeList = tradeQueryService.getAllTradesByMember(memberId);
+        List<Trade> tradeList = tradeQueryService.getAllTradesByMember(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -109,9 +113,9 @@ public class TradeController {
     @GetMapping("/my/like")
     @Operation(summary = "내가 찜한 품앗이 리스트 조회 API", description = "내가 찜한 품앗이 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findLikeTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ){
-        List<Trade> tradeList = tradeQueryService.getAllLikeTradesByMember(memberId);
+        List<Trade> tradeList = tradeQueryService.getAllLikeTradesByMember(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -119,10 +123,10 @@ public class TradeController {
     @GetMapping("/near/{trade_type}")
     @Operation(summary = "내 근처 품앗이 나눔/교환별 리스트 조회 API", description = "내 근처 게시글 중에서 나눔/교환별로 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findNearTradeByType(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_type") TradeType tradeType
     ){
-        List<Trade> tradeList = tradeQueryService.getNearTradesByType(memberId, tradeType);
+        List<Trade> tradeList = tradeQueryService.getNearTradesByType(member, tradeType);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -130,9 +134,9 @@ public class TradeController {
     @GetMapping("/near/recent")
     @Operation(summary = "내 근처 품앗이 최신 등록순 리스트 조회 API", description = "내 근처 게시글 중에서 최신 등록순으로 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findRecentTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ){
-        List<Trade> tradeList = tradeQueryService.getRecentTrades(memberId);
+        List<Trade> tradeList = tradeQueryService.getRecentTrades(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -140,9 +144,9 @@ public class TradeController {
     @GetMapping("/near/near-expiry")
     @Operation(summary = "내 근처 품앗이 소비기한 임박순 리스트 조회 API", description = "내 근처 게시글 중에서 소비기한 임박순으로 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findNearExpiryTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ) {
-        List<Trade> tradeList = tradeQueryService.getNearExpiryTrade(memberId);
+        List<Trade> tradeList = tradeQueryService.getNearExpiryTrade(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -150,9 +154,9 @@ public class TradeController {
     @GetMapping("/near/far-expiry")
     @Operation(summary = "내 근처 품앗이 소비기한 여유순 리스트 조회 API", description = "내 근처 게시글 중에서 소비기한 여유순으로 **프리뷰 리스트**를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findFarExpiryTrade(
-            @RequestParam Long memberId
+            @Parameter(name = "user", hidden = true) @AuthUser Member member
     ) {
-        List<Trade> tradeList = tradeQueryService.getFarExpiryTrade(memberId);
+        List<Trade> tradeList = tradeQueryService.getFarExpiryTrade(member);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -160,10 +164,10 @@ public class TradeController {
     @GetMapping("/other-trade")
     @Operation(summary = "다른 품앗이 둘러보기 API", description = "사용자의 동네 근처에서 최근 업로드된 다른 품앗이를 추천하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.TradePreviewListDTO> findOtherTrade(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @RequestParam Long tradeId
     ){
-        List<Trade> tradeList = tradeQueryService.getOtherTrade(memberId, tradeId);
+        List<Trade> tradeList = tradeQueryService.getOtherTrade(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_GET_LIST_OK, TradeConverter.toTradePreviewListDTO(tradeList));
     }
 
@@ -171,11 +175,11 @@ public class TradeController {
     @PostMapping("/like/{trade_id}")
     @Operation(summary = "품앗이 찜하기 API", description = "품앗이 찜하기 API 입니다.")
     public ApiResponse<TradeResponseDTO.LikeTradeResultDTO> likeTrade(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_id") Long tradeId
 
     ) {
-        LikeTrade likeTrade = tradeCommandService.createLike(memberId, tradeId);
+        LikeTrade likeTrade = tradeCommandService.createLike(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_LIKE_OK, TradeConverter.toLikeTradeResultDTO(likeTrade));
     }
 
@@ -183,10 +187,10 @@ public class TradeController {
     @DeleteMapping("/like/{trade_id}/delete")
     @Operation(summary = "품앗이 찜하기 취소 API", description = "품앗이 찜하기를 취소하는 API 입니다.")
     public ApiResponse<?> deleteLideTrade(
-            @RequestParam Long memberId,
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_id") Long tradeId
     ){
-        tradeCommandService.deleteLike(memberId, tradeId);
+        tradeCommandService.deleteLike(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_LIKE_DELETE_OK, null);
     }
 
@@ -194,9 +198,10 @@ public class TradeController {
     @GetMapping("/like/{trade_id}/count")
     @Operation(summary = "특정 품앗이의 찜하기 개수 조회 API", description = "특정 품앗이의 찜하기 개수를 조회하는 API 입니다.")
     public ApiResponse<TradeResponseDTO.LikeCountResultDTO> findLikeCount(
+            @Parameter(name = "user", hidden = true) @AuthUser Member member,
             @PathVariable("trade_id") Long tradeId
     ){
-        Trade trade = tradeQueryService.getTrade(tradeId);
+        Trade trade = tradeQueryService.getTrade(member, tradeId);
         return ApiResponse.onSuccess(SuccessStatus.TRADE_LIKE_COUNT_OK, TradeConverter.toLikeCountResultDTO(trade));
     }
 
