6f6815aea4176aae9331b04d441545d7898396d0
bingseok <qqq7384@naver.com>
2025-02-19 20:29:26 +0900

    남은 재료 뚝딱 API

--- src/main/java/com/backend/DuruDuru/global/repository/FridgeRepository.java
+++ src/main/java/com/backend/DuruDuru/global/repository/FridgeRepository.java
@@ -2,11 +2,12 @@ package com.backend.DuruDuru.global.repository;
 
 import com.backend.DuruDuru.global.domain.entity.Fridge;
 import com.backend.DuruDuru.global.domain.entity.Ingredient;
+import com.backend.DuruDuru.global.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FridgeRepository extends JpaRepository<Fridge, Long> {
-
-
+    Optional<Fridge> findByMember(Member member);
 }
